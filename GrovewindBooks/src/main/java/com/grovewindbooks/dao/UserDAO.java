1cd8d2f2114adb232a209fbb203438ee438dffdd
Jordon Kane <kanejord99@outlook.com>
2023-06-02 17:24:59 -0400

    test classes

---
+++ GrovewindBooks/src/main/java/com/grovewindbooks/dao/UserDAO.java
@@ -0,0 +1,50 @@
+package com.grovewindbooks.dao;
+
+import java.util.List;
+
+import com.grovewindbooks.entity.Users;
+
+import jakarta.persistence.EntityManager;
+
+public class UserDAO extends JpaDAO<Users> implements GenericDAO<Users> {
+
+	public UserDAO(EntityManager entityManager) {
+		super(entityManager);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Users create(Users user) {
+		return super.create(user);
+	}
+	
+	@Override
+	public Users update(Users t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Users get(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- GrovewindBooks/test/com/grovewindbooks/dao/UserDAOTest.java
+++ GrovewindBooks/test/com/grovewindbooks/dao/UserDAOTest.java
@@ -1,26 +1,36 @@
-package com.grovewindbooks.entity;
+package com.grovewindbooks.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.grovewindbooks.entity.Users;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class CategoryTest {
+public class UserDAOTest {
 
-	public static void main(String[] args) {
-		Category newCat = new Category("category test");
+	@Test
+	public void testCreateUsers() {
+		Users user1 = new Users();
+		user1.setEmail("wh8273e@email.com");
+		user1.setFullName("William Henry");
+		user1.setPassword("Tpass834");
+
 		//Instantiating EntityManager and EntityManagerFactory
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("GrovewindBooks");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
 		//Begin Transaction
 		entityManager.getTransaction().begin();
-		entityManager.persist(newCat);
+		entityManager.persist(user1);
 		
 		//Commit and close Transaction
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		entityManagerFactory.close();
-		
-		System.out.println("A category object was persisted.");
 	}
 
 }
