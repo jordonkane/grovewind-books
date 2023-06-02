47019f8d80761a8a110d5ce5b932d3083bb85f28
Jordon Kane <kanejord99@outlook.com>
2023-06-02 10:01:38 -0400

    added persistence.xml, Users.java, and UsersTest.java

---
+++ GrovewindBooks/test/UsersTest.java
@@ -0,0 +1,31 @@
+import com.grovewindbooks.entity.Users;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class UsersTest {
+
+	public static void main(String[] args) {
+		Users user1 = new Users();
+		user1.setEmail("wh8273e@email.com");
+		user1.setFullName("William Henry");
+		user1.setPassword("Tpass834");
+
+		//Instantiating EntityManager and EntityManagerFactory
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("GrovewindBooks");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		//Begin Transaction
+		entityManager.getTransaction().begin();
+		entityManager.persist(user1);
+		
+		//Commit and close Transaction
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
+		
+		System.out.println("A users object was persisted.");
+	}
+
+}
