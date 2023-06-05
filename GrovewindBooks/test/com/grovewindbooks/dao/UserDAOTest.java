29f5a11fa882d8d82469aa9d2bae609348c2bcf9
Jordon Kane <kanejord99@outlook.com>
2023-06-05 12:52:18 -0400

    refactor userDAO

--- GrovewindBooks/test/com/grovewindbooks/dao/UserDAOTest.java
+++ GrovewindBooks/test/com/grovewindbooks/dao/UserDAOTest.java
@@ -1,7 +1,9 @@
 package com.grovewindbooks.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.grovewindbooks.entity.Users;
@@ -11,24 +13,27 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class UserDAOTest {
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+	private static UserDAO userDAO;
+	
+	@BeforeClass
+	public static void setUpClass() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("GrovewindBooks");
+		entityManager = entityManagerFactory.createEntityManager();
+		
+		userDAO = new UserDAO(entityManager);
+	}
 
 	//Test createUsers method
 	@Test
 	public void testCreateUsers() {
 		Users user1 = new Users();
-		user1.setEmail("test@email.com");
-		user1.setFullName("Test Name");
-		user1.setPassword("testpassword");
+		user1.setEmail("z@email.com");
+		user1.setFullName("zion smith");
+		user1.setPassword("pass123");
 
-		//Instantiating EntityManager and EntityManagerFactory
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("GrovewindBooks");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		
-		UserDAO userDAO = new UserDAO(entityManager);
 		user1 = userDAO.create(user1);
-		
-		entityManager.close();
-		entityManagerFactory.close();
 		
 		//Test if UserId is greater than 0
 		assertTrue(user1.getUserId() > 0);
@@ -37,21 +42,17 @@ public class UserDAOTest {
 	@Test
 	public void testCreateUsersFieldNotSet() {
 		Users user1 = new Users();
-		user1.setEmail("test@email.com");
-		user1.setFullName("Test Name");
-		user1.setPassword("testpassword");
-
-		//Instantiating EntityManager and EntityManagerFactory
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("GrovewindBooks");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
-		UserDAO userDAO = new UserDAO(entityManager);
 		user1 = userDAO.create(user1);
-		
-		entityManager.close();
-		entityManagerFactory.close();
 		
 		//Test if UserId is greater than 0
 		assertTrue(user1.getUserId() > 0);
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		entityManager.close();
+		entityManagerFactory.close();
+		
 	}
 }
